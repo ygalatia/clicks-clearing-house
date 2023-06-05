76540b4e23fd46ba70b78dcf2c8331b0f79eb81c
yesayagm <ygalatia@gmail.com>
2023-06-05 11:55:54 +0200

    init project

---
+++ src/main/java/com/ids/clearinghouse/ClearinghouseApplication.java
@@ -0,0 +1,13 @@
+package com.ids.clearinghouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearinghouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearinghouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ids/clearinghouse/ClearinghouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ids.clearinghouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearinghouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
