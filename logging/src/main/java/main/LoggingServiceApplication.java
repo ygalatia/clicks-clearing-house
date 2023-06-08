a9f4fce18c4be6bcf9ead184cad7571a9156ddb7
yesayagm <ygalatia@gmail.com>
2023-06-08 09:18:34 +0200

    initiate logging module

---
+++ logging/src/main/java/main/LoggingServiceApplication.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingServiceApplication.class, args);
+    }
+}
