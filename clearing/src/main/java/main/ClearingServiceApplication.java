b81a8942439f1d5ed66e16528346bfeaa24a2689
yesayagm <ygalatia@gmail.com>
2023-06-08 09:17:57 +0200

    initiare clearing module

---
+++ clearing/src/main/java/main/ClearingServiceApplication.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClearingServiceApplication.class, args);
+    }
+}
