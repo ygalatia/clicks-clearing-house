3a9291abc87483548a77bfed3e29f82fe978db2e
yesayagm <ygalatia@gmail.com>
2023-06-25 18:41:28 +0200

    integrate Clearing service with database

---
+++ clearing/src/main/java/domain/model/ProcessRepository.java
@@ -0,0 +1,14 @@
+package domain.model;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProcessRepository extends MongoRepository<ClearingHouseProcess, String> {
+
+    Optional<ClearingHouseProcess> findById(String processId);
+    List<ClearingHouseProcess> findAll();
+
+}
